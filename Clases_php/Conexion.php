34a42665d4326376cfb77fb53146b984d242b68c
Bicri <isaac.bicri@gmail.com>
2021-06-30 17:05:56 -0500

    conexion establecida

--- Clases_php/Conexion.php
+++ Clases_php/Conexion.php
@@ -1,4 +1,26 @@
 <?php
 
+class Conexion{
 
+    private $host = "localhost";
+    private $user = "bicri";
+    private $password = "b1cr11999_";
+    private $db = "jugueteria";
+    private $con;
+    private $conexion;
+
+    public function conectar()
+    {
+        $this->conexion = "mysql:host=".$this->host.";dbname=".$this->db.";charset=utf8";
+        try{
+            $this->con = new PDO($this->conexion,$this->user,$this->password);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "conectado perrito";
+        }catch(Excepction $e)
+        {
+            $this->con = "Error en base de datos";
+            echo "Error en el servidor: ".$e->getMessage();
+        }
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
-//require_once ("Codigos_php/Conexion.php");
-require_once ("Clases_php/Juguete.php");
 
-$producto = new Juguete("123","balero",12.5,10,15,29,07,2021);
-echo "hola <br>";
+require_once ("Clases_php/Conexion.php");
 
-echo ($producto->toString());
+$conectate = new Conexion();
+$conectate->conectar();
+
+
+
+
+
+    
 
 ?>
