6a5a8bc30d0d83f05f3206409df611f8d2bc1c29
Isaac Bicri <51243242+Bicri@users.noreply.github.com>
2021-07-06 13:16:40 -0500

    Bicri (#10)
    * conexion establecida
    
    * primer json encode desde store procedure
    
    * Todos los productos y por coincidencias listo
    
    * ligeros cambios en mensajes xdxd
    
    * Inserta en tabla carrito y Fecha listo
    
    * commit antes de pdf
    
    * inicio de PDF y carpetas renombradas
    
    * PDF funcionando
    
    * pa k me deje hacer el pull request jsjs

--- Controlador/UsaLaFecha.php
+++ Controlador/UsaLaFecha.php
@@ -5,7 +5,8 @@ require_once ("../Modelo/Fecha.php");
 $fechas = new Fecha(0,0,0);
 $hoy = $fechas->getToday();
 $lunes = $fechas->getMonday();
-echo "El dia de hoy es: ".$hoy->generarSemana()." - ".$hoy->ToString();
+echo "El dia de hoy es: ".$hoy->ToString();
+echo "<br> Fecha formateada: ".$hoy->fechaFormateada();
 echo "<br>El Lunes de esta semana fue : ".$lunes->ToString();
 
 ?>
