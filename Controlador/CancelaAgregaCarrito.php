faf948d2e371ba39784631c71fd2290b2d5366b0
Bicri <isaac.bicri@gmail.com>
2021-07-08 16:39:52 -0500

    backend carrito terminado

---
+++ Controlador/CancelaAgregaCarrito.php
@@ -0,0 +1,36 @@
+<?php
+
+
+require_once ("../Modelo/Juguete.php");
+require_once ("../Modelo/Fecha.php");
+
+$accion = '{"Total":"21"}'; //Si total = 0 --> accion de eliminar carrito
+                           //Si total es mayor a 0 accion de efectuar venta
+
+
+
+$accion = json_decode($accion);
+
+$objJuguete = new Juguete(); //instancia a juguete
+$resp = 1; //captura la respuesta
+
+if($accion->Total == '0')
+{
+    $resp = $objJuguete->rollbackCarrito();
+}
+else
+{
+    $hoy = new Fecha(0,0,0);
+    $hoy = $hoy->getToday();
+    $resp = $objJuguete->efectuarVenta($hoy, $accion->Total);
+}
+
+echo $resp;
+/*
+si resp = 0 --> No hay error, accion de Agregar/Quitar ejecutada con exito
+si resp = -1 --> error en la bd
+*/
+
+
+
+?>
