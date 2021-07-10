62edf2df680384b8d911bd9df2190339404a856d
Isaac Bicri <51243242+Bicri@users.noreply.github.com>
2021-07-09 19:04:54 -0500

    Bicri (#17)
    * conexion establecida
    
    * primer json encode desde store procedure
    
    * Todos los productos y por coincidencias listo
    
    * ligeros cambios en mensajes xdxd
    
    * Inserta en tabla carrito y Fecha listo
    
    * commit antes de pdf
    
    * inicio de PDF y carpetas renombradas
    
    * PDF funcionando
    
    * pa k me deje hacer el pull request jsjs
    
    * problemas internos arreglados
    
    * ruta relativa corregida
    
    * agrega/quita una unidad del carrito
    
    * backend carrito terminado
    
    * Almacen sin funcionalidad borrar

---
+++ Controlador/Almacen.php
@@ -0,0 +1,117 @@
+<?php
+require_once("../Modelo/Juguete.php");
+require_once ("../Modelo/Fecha.php");
+//accion 1 --> Ingresar Nuevo
+//accion 2 --> Agregar stock
+//accion 3 --> Solicitar Juguete a editar (devuelve un juguete)
+//accion 4 --> Edicion terminada
+//accion 5 --> Agregar a lista
+//accion 6 --> Deseo borrar (ya paso el modal que le pregunta si esta seguro) --> devuleve el costo de su juguete
+//accion 7 --> No desea agregar el costo del juguete borrado a utilidades
+//accion 8 --> desea agregar el costo del juguete borrado a utilidades
+
+//Los datos en el objeto recibido dependen de la accion que se requiera, cuando no sea necesario
+//recibir algunos datos mandar sus atributos en vacio "" o simplemente no crear esa propiedad
+//$jugueteRecibido = '{"accion":"8","idNuevo":"10","idViejo":"9","nombre":"Batman","precio":"25.5","costo":"10","cantidad":2}';
+$jugueteRecibido = '{"accion":"5","idNuevo":"1","idViejo":"","nombre":"","precio":"","costo":"","cantidad":"2","anio":"","mes":"","dia":""}';
+
+$jugueteRecibido = json_decode($jugueteRecibido);
+
+
+$objJuguete = new Juguete();
+$hoy = new Fecha(0,0,0);
+$hoy = $hoy->getToday();
+
+$resp = 1; //captura la respuesta
+
+//---------------------------------- Agregar Nuevo Juguete --------------------------------------------
+// Ejemplo de juguete recibido:
+// $jugueteRecibido = '{"accion":"1","idNuevo":"","idViejo":"","nombre":"Batman","precio":"25.5","costo":"10","cantidad":"2"}';
+// $jugueteRecibido = '{"accion":"1","idNuevo":"123","idViejo":"","nombre":"Batman","precio":"25.5","costo":"10","cantidad":"2"}';
+if($jugueteRecibido->accion == "1")
+{
+    $resp = $objJuguete->NuevoJuguete($jugueteRecibido,$hoy);
+    echo $resp;
+    //Si resp = 0 --> Agregado con exito
+    //Si resp = 1 --> Juguete repetido ERROR
+    //Si resp = -1 --> Error en bd
+}
+
+
+
+
+//---------------------------------- Agregar Stock a Juguete --------------------------------------------
+// Ejemplo de juguete recibido:
+// $jugueteRecibido = '{"accion":"2","idNuevo":"123","idViejo":"","nombre":"","precio":"25.5","costo":"10","cantidad":"2"}';
+else if($jugueteRecibido->accion == "2")
+{
+    $resp = $objJuguete->AgregarStock($jugueteRecibido,$hoy);
+    echo $resp;
+    //Si resp = 0 --> Agregado con exito
+    //Si resp = -1 --> Error en bd
+}
+
+
+
+
+//---------------------------------- Solicitar Edicion de  Juguete --------------------------------------------
+// Ejemplo de juguete recibido:
+// $jugueteRecibido = '{"accion":"3","idNuevo":"123","idViejo":"","nombre":"","precio":"","costo":"","cantidad":""}';
+else if($jugueteRecibido->accion == "3")
+{
+    $resp = $objJuguete->SolicitarEdicion($jugueteRecibido);
+    print_r($resp);
+    //Si resp = 0 --> Array ( [codigo] => 1 [nombre] => Batman [precio] => 30.00 [existencia] => 4 [costo] => 10.00 [anio] => 2021 [mes] => 7 [dia] => 9 )
+    //Si resp = -1 --> Error en bd
+}
+
+
+
+
+//---------------------------------- Efectuando la Edicion del Juguete --------------------------------------------
+// Ejemplo de juguete recibido:
+// $jugueteRecibido = '{"accion":"4","idNuevo":"123","idViejo":"123","nombre":"Batman","precio":"35","costo":"15","cantidad":"10","anio":"2021","mes":"07","dia":"09"}';
+// $jugueteRecibido = '{"accion":"4","idNuevo":"81","idViejo":"123","nombre":"Batman","precio":"35","costo":"15","cantidad":"10","anio":"2021","mes":"07","dia":"09"}';
+// $jugueteRecibido = '{"accion":"4","idNuevo":"","idViejo":"123","nombre":"Batman","precio":"35","costo":"15","cantidad":"10","anio":"2021","mes":"07","dia":"09"}';
+else if($jugueteRecibido->accion == "4")
+{
+    $resp = $objJuguete->EditaProducto($jugueteRecibido);
+    echo $resp;
+    //Si resp = 0 --> Agregado con exito
+    //Si resp = 1 --> Juguete con id repetido ERROR
+    //Si resp = -1 --> Error en bd  
+}
+
+
+
+
+//---------------------------------- Agregando Juguete a Lista --------------------------------------------
+// Ejemplo de juguete recibido:
+// $jugueteRecibido = '{"accion":"5","idNuevo":"123","idViejo":"","nombre":"","precio":"","costo":"","cantidad":"10","anio":"","mes":"","dia":""}';
+else if($jugueteRecibido->accion == "5")
+{
+    $resp = $objJuguete->AgregarLista($jugueteRecibido);
+    echo $resp;
+    //Si resp = 0 --> Agregado con exito
+    //Si resp = -1 --> Error en bd  
+}
+
+
+else if($jugueteRecibido->accion == "6")
+{
+    echo "Devolviendo el costo del juguete a borrar";
+}
+else if($jugueteRecibido->accion == "7")
+{
+    echo "No agregar costo de eliminacion a utilidades";
+}
+else if($jugueteRecibido->accion == "8")
+{
+    echo "Agregar costo de eliminacion a utilidades";
+}
+else
+{
+    echo $resp = -2; //ERROR EN IF
+}
+
+?>
