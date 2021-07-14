9433a04f5379cadbde50486a88ed3ff1ecaec4fc
FigueroaD23 <83797577+FigueroaD23@users.noreply.github.com>
2021-07-14 13:50:18 -0500

    Angel (#34)
    * conexion establecida
    
    * primer json encode desde store procedure
    
    * Renderizando los productos desde la conexion a BD con JS, arreglando estilos en botones, corrigiendo clase active en link
    
    * header ya bien
    
    * Todos los productos y por coincidencias listo
    
    * ligeros cambios en mensajes xdxd
    
    * filtrado de productosCard por input y boton Buscar
    
    * Mandar objeto del carrito al archivo php
    
    * ver carrito primera parte y estructura
    
    * carrito funcionando primera fase (pintar en modal)
    
    * Carrito listo en front (falta modal de inputs)
    
    * todo en orden en modal carrito y añadir (estructura y listeners)
    
    * front funcionando con animaciones
    
    * carrito agregar producto con boton primera fase
    
    * carrito funcionando cuando enviaba todos los productos
    
    * carrito funcionando ENVIA SOLO UN PRODUCTO (revisión en php)
    
    * carrito funcionando excelente 1ra fase
    
    * recibiendo cambios
    
    * estilo en stock=0 rediseño de cards y de modal cantidad, arregla bug de que deja agregar más cantidad de la que hay en el segundo intento
    
    * carrito finalizado (falta el boton de efectuar compra)
    
    * administracion y modal universal hechos con estilos completos
    
    * agregar nuevo producto FASE 1
    
    * agregar nuevo producto listo
    
    * agregar nuevo/stock y editar listos
    
    * borrar primera fase error en el ID con letra
    
    * borrar con modal de confirmar (diferente boton si es 0 el costo)
    
    * conflictos arreglados a las 12:22am del lunes
    
    * ahora si ya bien los conflictos
    
    * ventas corregido (boton aumentar, actualizar y vaciar) -- admin terminado hasta borrar (falta agregar a lista futura) corrección en bootstrap modal confirmar y ejecución en local
    
    * modal diferente para lista futura
    
    * almacen listo con agregar a lista completo y estructura de los modales mejorada
    
    * estructura y diseño de la pagina 'lista de compras futuras' lista
    
    * arreglando boton efectua compra
    
    * Ultimos cambios al almacen
    
    * validacion de contraseña empezado
    
    * contrasena terminada con estilo en input
    
    * Lista de compras listo con todo ya
    
    * JuguetesTop terminada
    
    * gastos indirectos terminado
    
    * modal gastos en include
    
    * alertas listo y gastos mejorado
    
    Co-authored-by: Bicri <isaac.bicri@gmail.com>
    Co-authored-by: Isaac Bicri <51243242+Bicri@users.noreply.github.com>

--- Alertas.php
+++ Alertas.php
@@ -23,13 +23,43 @@
             <div class="Header-seccion" style=" flex-direction: row; justify-content: space-between; align-items: center;">
                 <h1 class="Titulo-seccion" id="tituloH" style=" margin:0;">Alertas</h1>
             </div>
+
+
+            <div class="Container-Elements" id="itemsAdministracion">
+                <table class="table table-hover">
+                    <thead style="position:sticky; top:78px; background: rgba(240, 248, 255,0.7); backdrop-filter:blur(25px);" id="thead">
+                        <tr>
+                            <th scope="col">#ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Existencia</th>
+                        </tr>
+                    </thead>
+
+                    <tbody id="elementosAlert">
+
+                    </tbody>
+                </table>
+
+            </div>
+
         </div>
     </div>
 
+    <template id="productoAlert">
+        <tr>
+            <td class="table-light" id="idProdTop">123456789asdf</td>
+            <td class="table-light" id="nomProdTop">PistolaNerf G 240 Naranja 24mm</td>
+            <td class="table-danger" id="cantidadTop">100</td>
+        </tr>
+    </template>
 
 
     <?php include_once("Layouts/modalGastos.php"); ?>
 
     <script src="src/js/gastos.js"></script>
+
+    <script src="src/js/alertas.js"></script>
+
 </body>
+
 </html>
