849d16841a8e30751faa18a908ef25f2ba845a90
unknown <angelnewrg4l@outlook.com>
2021-07-13 22:37:39 -0500

    modal gastos en include

--- JuguetesTop.php
+++ JuguetesTop.php
@@ -59,41 +59,9 @@
             <td class="table-light" id="ingresoTop">500.87</td>
         </tr>
     </template>
-    <!-- MODAL DE GASTOS -->
-    <div class="containerModal " style="align-items: flex-start; background: rgba(0, 0, 0, 0.5);" id="modalGastos">
-        <div class="modal-dialog" style="max-width:10000px; width:400px;" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Gastos del día</h5>
-                    <button type="button" class="btn btn-danger" id="cerrarModalGastos">Cancelar</button>
-                    </button>
-                </div>
-                <!-- <div class="modal-body" style="display:flex; justify-content: center; align-items:center; flex-direction: row; width:100%;"> -->
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Vigilancia</label>
-                            <input type="number" class="form-control" id="vigilanciaInput" placeholder="Introduce">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Comida</label>
-                            <input type="number" class="form-control" id="comidanput" placeholder="Introduce">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Otros</label>
-                            <input type="number" class="form-control" id="otrosInput" placeholder="Introduce">
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer" style="justify-content: center;">
-                    <button type="button" class="btn btn-primary" id="BTNgastos">Agregar gastos</button>
-                </div>
-            </div>
 
-        </div>
-    </div>
-    </div>
-    <!-- MODAL DE GASTOS -->
+    
+    <?php include_once("Layouts/modalGastos.php"); ?>
 
 
     <script src="src/js/Top.js"></script>
