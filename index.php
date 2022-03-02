1970e18bb023530d9a9085f87f1843b1638e91dd
Bicri <isaac.bicri@gmail.com>
2022-03-02 12:58:12 -0600

    Index corregido

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
 
-echo "pa que me deje hacer el pull jajaj";
+<!DOCTYPE html>
+<html lang="es" xml:lang="es">
+<head>
+    <title>Ventas</title>
+</head>
+<body>
+    <?php
+        
+        header("Location: ./Ventas.php", true, 301);
+        exit();
+    ?>
+</body>
+</html>
 
-?>
